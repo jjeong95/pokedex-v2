1952ad7a12ae235af9d6071ab3ff167153da8301
Jun Jeong <jun.jeong95@gmail.com>
2022-04-09 20:43:28 -0700

    deploy my first changes to my github repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <h2>Author: "Jun Jeong"</h2>
+      
+    </div>
+  );
+}
+
+export default App;
