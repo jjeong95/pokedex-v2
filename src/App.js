5e9133c1eef43f04ff7da3d31a29f5bccbac8d9e
Jun Jeong <jun.jeong95@gmail.com>
2022-04-11 23:01:15 -0700

    changed root

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         
-        <Route path ="/" element={<Main/>}>
+        <Route path ="/hello" element={<Main/>}>
         </Route>
       </Routes>
     </Router>
