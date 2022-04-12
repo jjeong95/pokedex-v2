b7d00c89e2203227a1427d62bc6b9599edcdaf6e
Jun Jeong <jun.jeong95@gmail.com>
2022-04-11 22:55:22 -0700

    added react deploy package

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Grid } from "@mui/material"
 import axios from "axios"
 import './PokemonCard.css'
+import TypeTag from '../TypeTag/TypeTag';
 
 export default function PokemonCard (data) {
     
@@ -19,6 +20,19 @@ export default function PokemonCard (data) {
 
         let colorURL = "https://pokeapi.co/api/v2/pokemon-species/" + pokemonID + "/";
 
+        let idArray = ""+pokemonID.split("");
+
+        if(idArray.length < 2){
+           idArray = ['0','0',pokemonID];
+        }
+
+        if(idArray.length < 4){
+            idArray = ['0',...pokemonID];
+        }
+
+        let threeDigitID = idArray.join('');
+        
+
         async function fetchPokemonData(){
             // await axios.get(url).then((response1) => {
             //     setPokemonData(response1.data);
@@ -33,6 +47,7 @@ export default function PokemonCard (data) {
             await axios.get(colorURL).then((response1) => {
                 axios.get(url).then((response2)=>{
                     response2.data.color = response1.data.color.name;
+                    response2.data.pokemonID = threeDigitID
                     setPokemonData(response2.data);
                 })
             })
@@ -49,17 +64,38 @@ export default function PokemonCard (data) {
             // })
 
         }
+
         fetchPokemonData();
     }, [])
 
-    console.log(pokemonData,' 47')
+    console.log(pokemonData, '70')
+
+    const capitalize = (s) => {
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    }
+
     return(
         
             Object.keys(pokemonData).length === 0 ? null :
             <Grid item xs={3} id="root">
                 <Grid container justifyContent="center" >
-                    <Grid item id="image" style={{backgroundColor: "#F2F2F2"}}>
-                        <img src={pokemonData.sprites.front_default}/>
+                    <Grid item xs={12} id="image-container" style={{backgroundColor: "#F2F2F2"}}>
+                        <img src={pokemonData.sprites.front_default} id="image"/>
+                    </Grid>
+                    <Grid item xs={12} id="pokemon-info">
+                        <Grid item id="pokemon-ID">
+                            {pokemonData.pokemonID}
+                        </Grid>
+                        <Grid item id="pokemon-Name">
+                            {capitalize(pokemonData.species.name)}
+                        </Grid>
+                        <Grid container>
+                            {
+                                pokemonData.types.map((data,index) => {
+                                    return <TypeTag type={data}/>
+                                })
+                            }
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
